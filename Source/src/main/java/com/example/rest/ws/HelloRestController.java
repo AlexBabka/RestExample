3dc63d1f8820d977b11da01bec9566a05cc1d6cf
AlexBabka <alex.babka@gmail.com>
2016-01-31 18:53:21 +0200

    Hello world with Spring MVC+REST

---
+++ Source/src/main/java/com/example/rest/ws/HelloRestController.java
@@ -0,0 +1,20 @@
+package com.example.rest.ws;
+
+import com.example.rest.bean.Greeting;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class HelloRestController {
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Greeting hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+        //returns the view name
+        return new Greeting(counter.incrementAndGet(), name);
+    }
+}
